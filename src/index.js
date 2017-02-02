b7c653e748974434f9fa19c9edca21115c69a61f
olegman <olegman.ru@gmail.com>
2017-02-02 17:15:49 +0300

    add support for passing arguments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { fork as sagaFork } from 'redux-saga/effects';
 
 // helper is Saga Helpers (takeEvery, takeLatest, throttle)
-export function forkActionWatcher(helper, actions, watcher) {
+export function forkActionWatcher(helper, actions, watcher, ...args) {
     if (!Array.isArray(actions)) {
         actions = [actions]
     }
     actions = actions.map(action => action.toString());
-    return sagaFork(helper, actions, watcher);
+    if (args.length) {
+        return sagaFork(helper, actions, watcher, ...args);
+    } else {
+        return sagaFork(helper, actions, watcher);
+    }
 }
